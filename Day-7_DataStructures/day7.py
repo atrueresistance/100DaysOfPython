2914f3e5e7fdaf2f2d378a8167054d8e879788b1
Frank Wiebenga <atrueresistance@gmail.com>
2018-05-06 13:18:13 -0500

    Day 7 start

---
+++ Day-7_DataStructures/day7.py
@@ -0,0 +1,30 @@
+# https://codechalleng.es/bites/promo/datastructures
+
+cars = {
+    'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
+    'Holden': ['Commodore', 'Captiva', 'Barina', 'Trailblazer'],
+    'Nissan': ['Maxima', 'Pulsar', '350Z', 'Navara'],
+    'Honda': ['Civic', 'Accord', 'Odyssey', 'Jazz'],
+    'Jeep': ['Grand Cherokee', 'Cherokee', 'Trailhawk', 'Trackhawk']
+}
+
+
+def get_all_jeeps():
+    pass
+
+
+def get_first_model_each_manufacturer():
+    """return a list of matching models (original ordering)"""
+    pass
+
+
+def get_all_matching_models(grep='trail'):
+    """return a list of all models containing the case insensitive
+       'grep' string which defaults to 'trail' for this exercise,
+       sort the resulting sequence alphabetically"""
+    pass
+
+
+def sort_car_models():
+    """sort the car models (values) and return the resulting cars dict"""
+    pass
